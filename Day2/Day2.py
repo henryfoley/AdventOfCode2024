39c96de3c746a5d5e10d57158fc877e62844de93
Henry Foley <henry.foley@mymail.champlain.edu>
2024-12-03 19:01:13 -0500

    Day 2 Complete

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -13,7 +13,7 @@ import re
 
 # Get contents of text file
 ROOT_DIR = Path(__file__).parent
-TEXT_FILE = ROOT_DIR / "example.txt"
+TEXT_FILE = ROOT_DIR / "input.txt"
 contents = TEXT_FILE.read_text()
 
 # Regex
@@ -21,26 +21,53 @@ regex_key = '^(\d+(?: \d+)*)$'
 rows = re.findall(regex_key, contents, re.MULTILINE)
 reports = [tuple(map(int, row.split())) for row in rows]
 
-
-
+# Initialize Variables
 iterator = 0
+increasing = True
+numSafe = len(reports)
+safe = True
+to_remove = []
 
+# Check if all levels have the same flow
 for report in reports:
-    safe = True
-    increasing = True
-    iterator += 1
+    prevIncreasing = increasing
     for i in range(len(report)):
         if i < len(report)-1:
             if report[i] < report[i+1]:
                 increasing = True
             else:
                 increasing = False
-        print(increasing)
-    # for num in report:
-    #     currentNum = num
-    #     previousNum = num
-    #     print(currentNum)
+        if increasing != prevIncreasing and i !=0 :
+            safe = False
+            break
+        else:
+            safe = True
+        prevIncreasing = increasing
+    if not safe:
+        numSafe -= 1
+        to_remove.append(report)
 
-    print("REPORT FINISHED")
-    if iterator == 1:
-        break
+for report in to_remove:
+    reports.remove(report)
+to_remove.clear()
+
+# Check number range
+for report in reports:
+    safe = True
+    for i in range(len(report)):
+        if i < len(report)-1:
+            difference = abs(report[i]-report[i+1])
+            if difference > 0 and difference < 4:
+                safe = True
+            else:
+                safe = False
+                break
+
+    if not safe:
+        to_remove.append(report)
+
+for report in to_remove:
+    reports.remove(report)
+
+# Output Number of Safe Reports
+print("Total Safe Reports: " + str(len(reports)))
