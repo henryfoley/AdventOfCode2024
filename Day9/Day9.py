5ad1e0a0cac0d8a7a68449942ea1046f013af1ba
Henry Foley <henry.foley@mymail.champlain.edu>
2024-12-11 17:17:12 -0500

    Day 9: WIP pt.II
    Testing out solution found on reddit. Mine is very close, but I'm not sure where the discrepancy is coming from

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -8,7 +8,7 @@
 6.) Find checksum of new block
 """
 
-# Imports
+# # Imports
 from pathlib import Path
 
 # Get contents of text file
@@ -55,8 +55,44 @@ for j, letter in enumerate(block):
     new_block.append(block[j])
 
 # Calculate the total by iterating over the new block
-total = 0
-for y, num in enumerate(new_block):
-    total += y * int(num)
+total = sum(i*int(num) for i,num in enumerate(new_block))
+
 
 print(f"Final Total: {total}")
+
+from collections import deque
+
+
+lengths = [int(num) for num in input]
+
+filled_grid = deque()
+moved_grid = deque()
+gaps = deque()
+
+cur_pos = 0
+for i,num in enumerate(lengths):
+    if i%2 == 0:
+        filled_grid.append([i//2,cur_pos,num])
+    else:
+        if num > 0:
+            gaps.append([cur_pos,num])
+    cur_pos += num
+
+while True:
+    gap_pos,gap_len = gaps.popleft()
+    file_id,file_pos,file_len = filled_grid.pop()
+    if gap_pos > file_pos:
+        filled_grid.append([file_id,file_pos,file_len])
+        break
+    if gap_len > file_len:
+        moved_grid.append([file_id,gap_pos,file_len])
+        gaps.appendleft([gap_pos+file_len,gap_len-file_len])
+    elif gap_len == file_len:
+        moved_grid.append([file_id,gap_pos,file_len])
+    else:
+        moved_grid.append([file_id,gap_pos,gap_len])
+        filled_grid.append([file_id,file_pos,file_len-gap_len])
+    
+final_grid = filled_grid + moved_grid
+answer = sum(num*(start*length+(length*(length-1))//2) for num,start,length in final_grid) # (start) + (start+1) + ... + (start+length-1)
+print(answer)
